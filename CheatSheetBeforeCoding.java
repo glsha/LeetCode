6078c7bf79f8996a017e092d053dd8c4cc864d44
Shashank Neupane <walkthrough26@gmail.com>
2026-02-05 22:18:20 -0600

    Cheat sheet for leetcode

---
+++ CheatSheetBeforeCoding.java
@@ -0,0 +1,15 @@
+// public class CheatSheetBeforeCoding {
+//     int evenNum = String.valueOf(num).length();
+//     maxCount = Math.max(maxCount, currentCount);
+//     Arrays.sort(nums);
+
+//     for (int i = 3; i >= 0; i--) {
+//         intArray[i + 1] = intArray[i];
+//     }
+
+//     for (int i = 4; i >= 2; i--){
+//         intArray[i + 1] = intArray[i];
+//     }
+
+
+
